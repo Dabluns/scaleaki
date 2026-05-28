7f75652143fd7e9119080815c9020fbbfefde724
Dabluns <leonamfilho40@gmail.com>
2026-05-28 16:25:11 -0300

    chore: remover pasta da extensao do chrome a pedido do usuario

--- extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // O Facebook tem bloqueios severos contra injeção no Main World e Monkey-patching
-  // de XMLHttpRequest/fetch, o que estava causando o React Error #185 (Loop de renderização).
-  // Portanto, a injeção de rede foi desativada.
-});
